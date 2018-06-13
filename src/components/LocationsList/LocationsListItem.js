f4de2078a6a0cfed1df8e8c20fae6204504928c6
aroksetx <aroksetxua@gmail.com>
2018-06-14 00:54:48 +0300

    add coordinates formatter

--- src/components/LocationsList/LocationsListItem.js
+++ src/components/LocationsList/LocationsListItem.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react'
-import { Text, View, TouchableOpacity} from 'react-native'
-import styles from './LocationListStyle'
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import styles from "./LocationListStyle";
 
 export default class LocationsListItem extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <View style={styles.locationsContainer} >
-            <TouchableOpacity >
-                <Text style={styles.locationItemTitle}>{this.props.title} </Text>
-                    <View style={styles.locationItemDescription}>
-                        <Text style={styles.locationDescriptionItem}>Latitude: {this.props.coordinates.latitude}</Text>
-                        <Text style={styles.locationDescriptionItem}>Longitude: {this.props.coordinates.longitude}</Text>
-                    </View>
-            </TouchableOpacity>
-            </View>
-        )
-    }
+  prettyCoordinates(coord) {
+    return coord.toString().substr(0, 6);
+  }
+
+  render() {
+    return (
+      <View style={styles.locationsContainer}>
+        <TouchableOpacity>
+          <Text style={styles.locationItemTitle}>{this.props.title} </Text>
+          <View style={styles.locationItemDescription}>
+            <Text style={styles.locationDescriptionItem}>
+              Latitude: {this.prettyCoordinates(this.props.coordinates.latitude)}
+            </Text>
+            <Text style={styles.locationDescriptionItem}>
+              Longitude: {this.prettyCoordinates(this.props.coordinates.longitude)}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
